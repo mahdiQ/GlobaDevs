11ba32b9a3c3e67d858873ed56e8f10f7f07923c
mahdi rabaa <mahdirabaa@outlook.com>
2019-06-11 15:28:28 +0300

    got to (create edit view)

--- TTtutorial/Controllers/StudentController.cs
+++ TTtutorial/Controllers/StudentController.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TTtutorial.Models;
 
 namespace TTtutorial.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: Student
-        public string Index()
+        IList<Student> studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+
+        public ActionResult Index()
         {
-            return "This is action method of Studentcontroller";
+            return View(studentList);
+        }
+
+        [HttpPost]
+        public ActionResult PostAction()
+        {
+            return View("Index");
+        }
+        public ActionResult Edit(int Id)
+        {
+            //Get the student from studentList sample collection for demo purpose.
+            //Get the student from the database in the real application
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            //write code to update student 
+            return RedirectToAction("Index");
+        }
+
+        [HttpPut]
+        public ActionResult PutAction()
+        {
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteAction()
+        {
+            return View("Index");
+        }
+
+        [HttpHead]
+        public ActionResult HeadAction()
+        {
+            return View("Index");
+        }
+
+        [HttpOptions]
+        public ActionResult OptionsAction()
+        {
+            return View("Index");
+        }
+
+        [HttpPatch]
+        public ActionResult PatchAction()
+        {
+            return View("Index");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post | HttpVerbs.Get)]
+        public ActionResult GetAndPostAction()
+        {
+            return RedirectToAction("Index");
         }
     }
 }
